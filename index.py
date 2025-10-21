fe532677844b8e56ed001e3e295509c352b3d5e7
carlosEA28 <carloseaduarte28@gmail.com>
2025-10-21 20:19:53 -0300

    remove forecast route

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ from fastapi import FastAPI
 from flight_server import flight_mcp
 from weather_server import weather_mcp
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # lifespan significa eventos que rodam durate a ainicialização e encerramento do app
 @contextlib.asynccontextmanager
@@ -17,6 +19,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.mount("/weather", weather_mcp.streamable_http_app())
 app.mount("/flight", flight_mcp.streamable_http_app())
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["http://localhost:4321"] se quiser limitar
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 if __name__ == "__main__":
